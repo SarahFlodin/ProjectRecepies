62cc3b8e6187db2ccb2c49755bb852ec90c66ed2
Sofie Johansson <sofie.johansson.sovde@gmail.com>
2022-12-07 11:35:36 +0100

    delete

---
+++ deleteComment.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once "functions.php";
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+$comments = [];
+
+if (file_exists("comments.json")) {
+    $json = file_get_contents("comments.json");
+    $comments = json_decode($json, true);
+}
+
+$requestJson = file_get_contents("php://input");
+$requestData = json_decode($requestJson, true);
+
+if ($requestMethod == "DELETE") {
+    if (!isset($requestData["commentId"])) {
+        $error = ["error" => "Bad Request"];
+        sendStatus($error, 400);
+    }
+
+    $commentId = $requestData["commentId"];
+
+    foreach($comments as $number => $comment) {
+        if ($comment["commentId"] == $commentId) {
+            array_splice($comments, $number, 1);
+            $json = json_encode($comments, JSON_PRETTY_PRINT);
+            file_put_contents("comments.json", $json);
+            sendStatus($commentId);
+        }
+    }
+
+    $error = ["error" => "Not Found"];
+    sendStatus($error, 404);
+}
