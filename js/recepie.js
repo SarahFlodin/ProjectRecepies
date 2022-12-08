3b0579422ce8fa5deee03265b6d63a0d683532aa
claralindbloms <97987325+claralindbloms@users.noreply.github.com>
2022-12-08 23:48:45 +0100

    start of build dish

--- js/recepie.js
+++ js/recepie.js
@@ -1,47 +1,57 @@
-// function buildDish(dishId){
-//     let dishResult = document.querySelector("#wrapper");
-//     let dishIntroduction = document.createElement("div");
+function buildDish(){
+        //BYT id till dish elelr dishId
+        //koppla till förstasidan :)
+        let dishResult = document.querySelector("#wrapper");
+        let dishIntroduction = document.createElement("div");
+        dishIntroduction.classList.add("dishIntroduction");
+        dishResult.append(dishIntroduction);
+        
+        dishIntroduction.innerHTML = `
+        <h1>${id.name}</h1>
+        <p>${id.time}  ${id.price}</p>
+        <p>${id.info}</p>
+        `;
+
+        let dishIngredients = document.createElement("div");
+        dishIngredients.classList.add("dishIngredients");
+        dishResult.append(dishIngredients);
+        
+        dishIngredients.innerHTML =`
+        <h1>Ingredienser</h1>
+        <p>${id.ingridients}</p>
+        `;
+
+        let dishTodo = document.createElement("div");
+        dishTodo.classList.add("dishTodo");
+        dishResult.append(dishTodo);
+        
+        dishTodo.innerHTML =`
+        <h1>Gör såhär:</h1>
+        <p>${id.todo}</p>
+        `;
     
-//     dishIntroduction.innerHTML = `
-//     <h1>${dish.name}</h1>
-//     <p>${dish.time}  ${dish.price}</p>
-//     <p>${dish.info}</p>
-   
-//     `;
-// }
+        let dishAllergies = document.createElement("div");
+        dishAllergies.classList.add("dishAllergies");
+        dishResult.append(dishAllergies);
+        
+        dishAllergies.innerHTML =`
+        <h1>Allergener</h1>
+        <p>${id.allergies}</p>
+        `;
+     
+    
+    
+    
+ }
+
+ //buildDish();
     //ha innanför innerHTML med
     //dish.pictureurl dish.rating button = heart
     //(samma som heartfunktionen på startsidan)
     
-   /* dishIntroduction.classList.add(dishIntroduction);
-    dishResult.append(dishIntroduction);
+   /* 
     
-    let dishIngredients = document.createElement("div");
-    dishIngredients.innerHTML =`
-    <h1>Ingredienser</h1>
-    <p>${dish.ingridients}</p>
-    `;
-    //eller ingrid
-
-    dishIngredients.classList.add(dishIngredients);
-    dishResult append (dishIngredients)
     
-    let dishTodo create div
-    
-    dishTodo.innerHTML =`
-    
-    Gör såhär:
-    dish.todo`
-    dishTodo classList add (dishTodo)
-    dishResult append (dishTodo)
-    
-    let dishAllergies create div
-    
-    dishAllergies.innerHTML =`
-    
-    Allergener dish.allergies`
-    dishAllergies classList add (dishAllergies)
-    dishResult append (dishAllergies)
     
     här får vi rendera kommentarerna för varje recept
     let dishComments = document.createElement(“div”)
