ca669cfed2156c0553639f7ec1e34221f26502e6
claralindbloms <97987325+claralindbloms@users.noreply.github.com>
2023-01-07 12:05:59 +0100

    test

--- deleteFavorite.php
+++ deleteFavorite.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set("display_errors", 1);
+
 require_once "functions.php";
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
@@ -7,30 +9,35 @@ $favorites = [];
 
 if (file_exists("user.json")) {
     $json = file_get_contents("user.json");
-    $favorites = json_decode($json, true);
+    $users = json_decode($json, true);
 }
 
 $requestJson = file_get_contents("php://input");
 $requestData = json_decode($requestJson, true);
 
 if ($requestMethod == "DELETE") {
+    $userId = $requestData["userId"];
     
-    $favoritesNum = $requestData["favorites"];
+    foreach($users as $user){
+        if($user["userId"] == $userId){
 
-    if (!isset($favoritesNum)) {
-        $error = ["error" => "Bad Request"];
-        sendStatus($error, 400);
-    }
+            $favoritesNum = $requestData["favorites"];
 
-    foreach($favorites as $favorite) {
-        if($favorite["favorites"] == $favoritesNum) {
-            array_splice($favorites, 1);
-            $json = json_encode($favorites, JSON_PRETTY_PRINT);
-            file_put_contents("user.json", $json);
-            sendStatus($favoritesNum);
+            if (!isset($favoritesNum, $userId)) {
+                $error = ["error" => "Bad Request"];
+                sendStatus($error, 400);
+            }
+
+            foreach($favorites as $favorite) {
+                if($favorite["favorites"] == $favoritesNum) {
+                    array_splice($favorites, 1);
+                    $json = json_encode($favorites, JSON_PRETTY_PRINT);
+                    file_put_contents("user.json", $json);
+                    sendStatus($favoritesNum);
+                }
+            }
         }
     }
-
     $error = ["error" => "Not Found"];
     sendStatus($error, 404);
 }
