bcc24eaf84641bc655f3f3c2eecbc8348089c932
claralindbloms <97987325+claralindbloms@users.noreply.github.com>
2023-01-10 18:57:26 +0100

    add favorite

--- addFavorite.php
+++ addFavorite.php
@@ -7,6 +7,7 @@ require_once "functions.php";
 $requestedMethod = $_SERVER["REQUEST_METHOD"];
 
 $users = [];
+
 if (file_exists("user.json")) {
     $json = file_get_contents("user.json");
     $users = json_decode($json, true);
@@ -15,26 +16,23 @@ if (file_exists("user.json")) {
 $requestJson = file_get_contents("php://input");
 $requestedData = json_decode($requestJson, true);
 
-if($requestedMethod == "DELETE"){
+if($requestedMethod == "POST"){
 
     $dishId = $requestedData["dishId"];
     $userId = $requestedData["userId"];
 
     if(isset($dishId, $userId)){
         foreach($users as $number => $user){
-           if($number == $userId - 1){
+            if($number == $userId - 1){
                 $favorites = $user["favorites"];
-                foreach($favorites as $index => $favorite){
-                    if($favorite == $dishId){
-                        array_splice($favorites, $index, 1);
-                        $updated = $favorites;
-                        $user["favorites"] = $updated;
-                        $users[$number] = $user;
+                array_push($favorites, $dishId);
+                $updated = $favorites;
+                var_dump($dishId);
+                $user["favorites"] = $updated;
+                $users[$number] = $user;
                         
-                        $json = json_encode($users, JSON_PRETTY_PRINT);
-                        file_put_contents("user.json", $json);
-                    }
-                }
+                $json = json_encode($users, JSON_PRETTY_PRINT);
+                file_put_contents("user.json", $json); 
             }
         }
 
